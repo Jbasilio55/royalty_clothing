95d023babb1e35c463d611c433f20dbfc6d706d2
Jbasilio55 <j.l.basilio55@gmail.com>
2025-05-29 23:23:53 -0400

    adding cart context and shopping icon as well as cart dropdown

--- royalty-clothing/src/main.jsx
+++ royalty-clothing/src/main.jsx
@@ -3,13 +3,19 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.scss";
 import App from "./App.jsx";
-import { UserProvider } from "./context/user.context.jsx";
+import { UserProvider } from "./context/user.context";
+import { ProductsProvider } from "./context/products.context";
+import { CartProvider } from "./context/cart.context.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </StrictMode>
