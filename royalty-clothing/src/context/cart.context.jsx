f8642df5ce84ca8178739b3360283d7465fd7014
Jbasilio55 <j.l.basilio55@gmail.com>
2025-05-30 16:09:12 -0400

    added cart-item component and added basic styling also conenced it with cart-context

--- royalty-clothing/src/context/cart.context.jsx
+++ royalty-clothing/src/context/cart.context.jsx
@@ -1,12 +1,20 @@
 import { createContext, useState } from "react";
 
+const addCartItem = (cartItems, productToAdd) => {};
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const [carItem, setCartItem] = useState([]);
+
+  const addItemToCart = (productToAdd) => {};
+
   const value = { isCartOpen, setIsCartOpen };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
