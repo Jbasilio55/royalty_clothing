96a0353e94dcc47306ec4705bbe2574d894cf8fb
Jbasilio55 <j.l.basilio55@gmail.com>
2025-06-01 14:37:40 -0400

    added the styling and the visual items to check out

--- royalty-clothing/src/routes/checkout/checkout.component.jsx
+++ royalty-clothing/src/routes/checkout/checkout.component.jsx
@@ -3,30 +3,35 @@ import { CartContext } from "../../context/cart.context";
 
 import "./checkout.styles.scss";
 
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+
 const Checkout = () => {
   const { cartItems, addItemToCart, removeItemFromCart } =
     useContext(CartContext);
 
   return (
-    <div>
-      <h1>I am the checkout</h1>
-      <div>
-        {cartItems.map((cartItem) => {
-          const { id, name, quantity } = cartItem;
-          return (
-            <div>
-              <h2>{name}</h2>
-              <span>{quantity}</span>
-              <br />
-              <span onClick={() => removeItemFromCart(cartItem)}>
-                decrement
-              </span>
-              <br />
-              <span onClick={() => addItemToCart(cartItem)}>increment</span>
-            </div>
-          );
-        })}
+    <div className="checkout-container">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>Product</span>
+        </div>
+        <div className="header-block">
+          <span>Description</span>
+        </div>
+        <div className="header-block">
+          <span>Quantity</span>
+        </div>
+        <div className="header-block">
+          <span>Price</span>
+        </div>
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
       </div>
+      {cartItems.map((cartItem) => {
+        return <CheckoutItem key={cartItem.id} cartItem={cartItem} />;
+      })}
+      <span className="total">Total: 0</span>
     </div>
   );
 };
